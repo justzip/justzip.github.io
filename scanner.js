5cd44edaad2ee680be1d3005f9204053e68ce97d
justzip <146394163+justzip@users.noreply.github.com>
2024-02-01 11:23:51 +0100

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,41 +1,42 @@
-// Attente que le DOM soit complètement chargé
 document.addEventListener('DOMContentLoaded', function () {
-    // Initialisation de Quagga pour le scan de codes-barres
+    // Configuration initiale de Quagga pour le scan de codes-barres
     Quagga.init({
         inputStream: {
             name: "Live",
             type: "LiveStream",
-            target: document.querySelector('#interactive'), // Spécifie où afficher la vidéo
+            target: document.querySelector('#interactive'), // Cible l'élément pour le flux vidéo
             constraints: {
-                facingMode: "environment" // Utilise la caméra arrière pour les appareils mobiles
+                facingMode: "environment", // Préfère la caméra arrière sur les appareils mobiles
             },
         },
         decoder: {
-            readers: ["ean8_reader", "ean13_reader"], // Types de codes-barres à lire
+            readers: ["ean8_reader", "ean13_reader"], // Spécifie les types de codes-barres à lire
         },
     }, function (err) {
         if (err) {
             console.error(err);
+            alert('Error starting Quagga:\n' + err.message);
             return;
         }
+        // Affichage d'un message console une fois l'initialisation réussie
         console.log("Initialization finished. Ready to start");
         Quagga.start();
     });
 
-    // Gestion des scans réussis
+    // Écoute des scans réussis et affichage du code dans un champ input
     Quagga.onDetected(function (data) {
         var code = data.codeResult.code;
         console.log("Barcode detected and read:", code);
-        document.getElementById('barcode').value = code; // Affiche le code-barres dans l'élément input
+        document.getElementById('barcode').value = code; // Affiche le code-barres détecté
     });
 });
 
-// Fonction pour arrêter Quagga
+// Fonction pour arrêter Quagga, peut être utilisée si nécessaire
 function stopQuagga() {
     Quagga.stop();
 }
 
-// Ajout d'un écouteur d'événements sur le bouton de redémarrage pour redémarrer le scan
+// Gestion du redémarrage de Quagga via un bouton, si inclus dans l'interface utilisateur
 document.getElementById('restartBtn').addEventListener('click', function () {
-    Quagga.start(); // Redémarre Quagga
+    Quagga.start(); // Redémarre Quagga pour un nouveau scan
 });
